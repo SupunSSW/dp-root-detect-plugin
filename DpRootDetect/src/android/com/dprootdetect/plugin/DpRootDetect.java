5caefebc43efe19cbf5afd4d212159bcb96f0744
supunssw <ssadeepa7@gmail.com>
2023-01-04 01:13:21 +0530

    first commit

---
+++ DpRootDetect/src/android/com/dprootdetect/plugin/DpRootDetect.java
@@ -0,0 +1,39 @@
+package com.dprootdetect.plugin;
+
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.Manifest;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+import android.telephony.TelephonyManager;
+
+import com.lankaclear.justpay.LCTrustedSDK;
+import com.lankaclear.justpay.callbacks.CreateIdentityCallback;
+import com.lankaclear.justpay.callbacks.SignMessageCallback;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class DpRootDetect extends CordovaPlugin {
+
+    @Override
+    public boolean execute(
+            String action,
+            JSONArray args,
+            CallbackContext callbackContext)
+            throws JSONException {
+        if (action.equals("coolMethod")) {
+            String echo = args.getString(0);
+            return true;
+        }
+        return false;
+    }
+
+}
